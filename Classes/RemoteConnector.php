df6fb974d5fc2cd39fd1a41ae6029b8f4221c949
Nicola Fabiani <>
2018-02-16 14:14:50 +0100

    Add regular expression to check validity of the domain name

--- Classes/RemoteConnector.php
+++ Classes/RemoteConnector.php
@@ -5,6 +5,7 @@ class RemoteConnector
     protected $url; // hold the URL
     protected $remoteFile;  // hold the content of the file
     protected $error; // hold the error message
+    protected $urlParts;
 
     public function __construct ($url)
     {
@@ -23,7 +24,9 @@ class RemoteConnector
     {
         $flags = FILTER_FLAG_SCHEME_REQUIRED | FILTER_FLAG_HOST_REQUIRED;
         $urlOK = filter_var($this->url, FILTER_VALIDATE_URL, $flags);
-        if (!$urlOK)
+        $this->urlParts = parse_url($this->url);
+        $domainOK = preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $this->url);
+        if (!$urlOK || !$domainOK)
             throw new Exception($this->url . ' is not a valid URL');
     }
 
