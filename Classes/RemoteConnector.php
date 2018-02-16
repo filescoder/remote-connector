945629a6c3e144e7c213c8fc6c6f4c4e631f7861
Nicola Fabiani <>
2018-02-16 17:03:30 +0100

    Define useSocket method

--- Classes/RemoteConnector.php
+++ Classes/RemoteConnector.php
@@ -11,9 +11,9 @@ class RemoteConnector
     {
         $this->url = $url;
         $this->checkURL();
-        if (ini_get('allow_url_fopen')) {
+        if (ini_get('allow_url_open')) {
             $this->accessDirect();
-        } else if (function_exists('curl_init')) {
+        } else if (function_exists('cur_init')) {
             $this->useCurl();
         } else {
             $this->useSocket();
@@ -68,7 +68,62 @@ class RemoteConnector
 
     protected function useSocket()
     {
-        echo 'Will use a socket connection';
+        $port = isset($this->urlParts['port']) ? $this->urlParts['port'] : 80;
+        $remote = @ fsockopen($this->urlParts['host'], $port, $errno, $errstr, 30);
+
+        if (!$remote) {
+            $this->remoteFile = false;
+            $this->error = "Couldn't create a socket connection ";
+            if ($errstr)
+                $this->error .= $errstr;
+            else
+                $this->error .= "check the domain name or IP address";
+        } else {
+            if (isset($this->urlParts['query'])) {
+                $path = $this->urlParts['path'] . '?' . $this->urlParts['query']; 
+            } else {
+                $path = $this->urlParts['path'];
+            }
+            $out = "GET $path HTTP/1.1\r\n";
+            $out .= "Host: {$this->urlParts['host']}\r\n";
+            $out .= "Connection: Close\r\n\r\n";
+
+            fwrite($remote, $out);
+
+            $this->remoteFile = stream_get_contents($remote);
+            if ($this->remoteFile)
+                $this->removeHeaders();
+
+            fclose($remote);
+        }
+    }
+
+    protected function removeHeaders()
+    {
+        $parts = preg_split('#\r\n\r\n|\n\n#', $this->remoteFile); // Split the string based on double new line
+        if (is_array($parts)) {
+            $headers = array_shift($parts);
+            $file = implode("\n\n", $parts);
+            if (preg_match('#HTTP/1\.\d\s+(\d{3})#', $headers, $m)) {
+                $this->status = $m[1];
+            }
+            if (preg_match('#Content-Type:([^\r\n]+)#i', $headers, $m)) {
+                if (stripos($m[1], 'xml') !== false || stripos($m[1], 'html') !== false) {
+                    if (preg_match('/<.+>/s', $file, $m)) {
+                        $this->remoteFile = $m[0];
+                    } else {
+                        $this->remoteFile = trim($file);
+                    }
+                } else {
+                    $this->remoteFile = trim($file);
+                }
+            } 
+        }
+    }
+
+    public function getErrorMessage()
+    {
+        return $this->error;
     }
     
 }
